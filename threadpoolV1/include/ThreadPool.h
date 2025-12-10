62e4a4fdc8c74baa20ef7f9c37c7a508d009c881
njlookforward <njlookforward@163.com>
2025-12-11 00:09:16 +0800

    uncomplete day3's test

--- threadpoolV1/include/ThreadPool.h
+++ threadpoolV1/include/ThreadPool.h
@@ -7,7 +7,7 @@
 #include <functional>
 #include <atomic>
 #include <mutex>
-#include <future>   // 得到异步结果
+#include <future> // 得到异步结果
 #include <condition_variable>
 #include <utility>
 #include <stdexcept>
@@ -18,17 +18,27 @@ public:
     ThreadPool(size_t threadNum);
     ~ThreadPool();
 
+    // 线程池不应该被拷贝，是线程唯一的
+    ThreadPool(const ThreadPool &) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
+
+    // 添加任务的接口：会返回异步执行函数所返回的结果，所以需要使用std::package_task与std::future与lambda表达式
+    // 这个任务队列更加强大，能够添加任何类型的函数，并异步返回其结果
+    template <typename F, typename... Args>
+    auto enqueue(F &&func, Args &&...args)
+        -> std::future<typename std::invoke_result<F, Args...>::type>;
+
     // 返回此时的线程池的运行状态
-    bool isStopped() const {    return _stop;   }
+    bool isStopped() const { return _stop; }
 
 private:
-    void workerThread();    // 工作线程应该执行的函数
+    void workerThread(); // 工作线程应该执行的函数
 
-    std::vector<std::thread> _workers;      // 工作线程
-    std::queue<std::function<void()>> _taskQueue;   // 任务队列
-    std::mutex _queue_mtx;      // 线程安全地访问任务队列
-    std::condition_variable _condition;     // 条件变量，同步进行生产者消费者模型
-    std::atomic<bool> _stop{false};    // 线程池是否停止标志位
+    std::vector<std::thread> _workers;            // 工作线程
+    std::queue<std::function<void()>> _taskQueue; // 任务队列
+    std::mutex _queue_mtx;                        // 线程安全地访问任务队列
+    std::condition_variable _condition;           // 条件变量，同步进行生产者消费者模型
+    std::atomic<bool> _stop{false};               // 线程池是否停止标志位
 };
 
 #endif
--- threadpoolV1/src/ThreadPool.cpp
+++ threadpoolV1/src/ThreadPool.cpp
@@ -7,10 +7,11 @@ ThreadPool::ThreadPool(size_t threadnum)
     std::cout << "ThreadPool的构造函数被调用，threadNum = " << threadnum << std::endl;
 
     // 创建对应数量的线程池
-    for(size_t i = 0; i < threadnum; ++i)
+    for (size_t i = 0; i < threadnum; ++i)
     {
         _workers.emplace_back(
-            [this] {    this->workerThread();   }   // 可以添加lambda表达式作为线程的执行函数
+            [this]
+            { this->workerThread(); } // 可以添加lambda表达式作为线程的执行函数
         );
 
         // 多种不同的方法可以创建worker的工作线程
@@ -32,9 +33,9 @@ ThreadPool::~ThreadPool()
     _condition.notify_all();
 
     // 增强for循环将所有的工作线程回收
-    for(auto &worker : _workers)
+    for (auto &worker : _workers)
     {
-        if(worker.joinable())
+        if (worker.joinable())
         {
             worker.join();
         }
@@ -54,12 +55,11 @@ void ThreadPool::workerThread()
         {
             std::unique_lock<std::mutex> lock(_queue_mtx);
 
-            _condition.wait(lock, [this] {
-                return this->_stop || !this->_taskQueue.empty();
-            });
+            _condition.wait(lock, [this]
+                            { return this->_stop || !this->_taskQueue.empty(); });
 
             // 需要判断是停止线程池，还是能够获取新的任务
-            if(_stop && _taskQueue.empty())
+            if (_stop && _taskQueue.empty())
             {
                 // 线程池终止
                 return;
@@ -74,13 +74,53 @@ void ThreadPool::workerThread()
         {
             task();
         }
-        catch(const std::exception & e)
+        catch (const std::exception &e)
         {
             std::cerr << "任务执行异常" << std::endl;
         }
-        catch(...)
+        catch (...)
         {
             std::cerr << "任务执行过程中出现了未知异常" << std::endl;
         }
     }
 }
+
+// 获得可调用函数后，因为需要得到异步回调的执行结果，所以需要使用package_task来包裹可执行函数
+// 但是函数对象，可能是不同的返回类型，因此需要使用lambda表达式进行包裹
+template <class F, class... Args>
+auto ThreadPool::enqueue(F &&func, Args&&...args)
+    -> std::future<typename std::invoke_result<F, Args...>::type>
+{
+    // 判断func必须是有效的
+    if(!func)
+    {
+        // 这是空函数，没有任何意义
+        throw std::runtime_error("必须传入有效的函数对象");
+    }
+
+    using resultType = typename std::invoke_result<F, Args...>::type;
+
+    // 使用package_task修饰要被执行的函数，因为也是一个对象，需要被跨线程共享
+    // 因此申请在堆空间，然后使用智能指针包裹
+    std::shared_ptr<std::packaged_task<resultType()>> task = 
+            std::make_shared<std::packaged_task<resultType()>>(
+                // BUG: 这里是不对的，被包裹的函数对象必须是与模板类型一样的，因此需要使用std::bind绑定
+                std::bind(std::forward<F>(func), std::forward<Args>(args)...)
+            );
+    std::future<resultType> result = task->get_future();
+
+    // 接下来就是正常的生产者流程了，但是需要先判断——stop的状态
+    if(_stop)
+    {
+        throw std::runtime_error("此时线程池已经结束了，不应该再投放任务");
+    }
+
+    std::unique_lock<std::mutex> lock(_queue_mtx);
+    _taskQueue.emplace(
+        [task] {    (*task)();  }   // lambda表达式太强大了，直接修改了函数的返回值
+    );
+    _condition.notify_one();
+    lock.unlock();
+
+    return result;
+} 
